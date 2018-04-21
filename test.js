b6ba7e90c809b6a35855ec2619e6db4562d15860
Titus Wormer <tituswormer@gmail.com>
2018-04-21 12:13:31 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var hexadecimal = require('.');
+var test = require('tape')
+var hexadecimal = require('.')
 
-test('hexadecimal(character)', function (t) {
-  t.ok(hexadecimal('a'));
-  t.ok(hexadecimal('F'));
-  t.ok(hexadecimal('0'));
-  t.ok(hexadecimal('a'.charCodeAt(0)));
-  t.notOk(hexadecimal('G'));
-  t.notOk(hexadecimal('a'.charCodeAt(0) - 1));
-  t.notOk(hexadecimal('f'.charCodeAt(0) + 1));
-  t.notOk(hexadecimal('A'.charCodeAt(0) - 1));
-  t.notOk(hexadecimal('F'.charCodeAt(0) + 1));
-  t.notOk(hexadecimal('0'.charCodeAt(0) - 1));
-  t.notOk(hexadecimal('9'.charCodeAt(0) + 1));
-  t.notOk(hexadecimal('g'.charCodeAt(0)));
-  t.notOk(hexadecimal('💩'));
+test('hexadecimal(character)', function(t) {
+  t.ok(hexadecimal('a'))
+  t.ok(hexadecimal('F'))
+  t.ok(hexadecimal('0'))
+  t.ok(hexadecimal('a'.charCodeAt(0)))
+  t.notOk(hexadecimal('G'))
+  t.notOk(hexadecimal('a'.charCodeAt(0) - 1))
+  t.notOk(hexadecimal('f'.charCodeAt(0) + 1))
+  t.notOk(hexadecimal('A'.charCodeAt(0) - 1))
+  t.notOk(hexadecimal('F'.charCodeAt(0) + 1))
+  t.notOk(hexadecimal('0'.charCodeAt(0) - 1))
+  t.notOk(hexadecimal('9'.charCodeAt(0) + 1))
+  t.notOk(hexadecimal('g'.charCodeAt(0)))
+  t.notOk(hexadecimal('💩'))
 
-  t.end();
-});
+  t.end()
+})
