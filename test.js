7d9083c583eeebd3cc918b2e8e99d0e266a9f16d
Titus Wormer <tituswormer@gmail.com>
2018-04-21 12:11:56 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var hexadecimal = require('./');
+var hexadecimal = require('.');
 
 test('hexadecimal(character)', function (t) {
   t.ok(hexadecimal('a'));
