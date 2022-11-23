c05c86af8c617d45fd7a1dd8ce9a5895dede8659
Titus Wormer <tituswormer@gmail.com>
2022-11-23 13:06:41 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {isHexadecimal} from './index.js'
 
-test('isHexadecimal(character)', function (t) {
+test('isHexadecimal(character)', function () {
   const _0 = 0x30
   const _9 = 0x39
   const A = 0x41
   const F = 0x46
   const a = 0x61
   const f = 0x66
-  t.ok(isHexadecimal('a'))
-  t.ok(isHexadecimal('F'))
-  t.ok(isHexadecimal('0'))
-  t.ok(isHexadecimal(a))
-  t.notOk(isHexadecimal('G'))
-  t.notOk(isHexadecimal(a - 1))
-  t.notOk(isHexadecimal(f + 1))
-  t.notOk(isHexadecimal(A - 1))
-  t.notOk(isHexadecimal(F + 1))
-  t.notOk(isHexadecimal(_0 - 1))
-  t.notOk(isHexadecimal(_9 + 1))
-  t.notOk(isHexadecimal('💩'))
 
-  t.end()
+  assert.ok(isHexadecimal('a'))
+  assert.ok(isHexadecimal('F'))
+  assert.ok(isHexadecimal('0'))
+  assert.ok(isHexadecimal(a))
+  assert.ok(!isHexadecimal('G'))
+  assert.ok(!isHexadecimal(a - 1))
+  assert.ok(!isHexadecimal(f + 1))
+  assert.ok(!isHexadecimal(A - 1))
+  assert.ok(!isHexadecimal(F + 1))
+  assert.ok(!isHexadecimal(_0 - 1))
+  assert.ok(!isHexadecimal(_9 + 1))
+  assert.ok(!isHexadecimal('💩'))
 })
